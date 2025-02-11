357fcb6c2a98b403b89fefc61436f1618a86e3df
mannarmannan <mannarmannan02@gmail.com>
2025-02-11 15:03:04 +0530

    v18

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
-#Importing the required libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 import joblib
+import hashlib
 
-#Loading the dataset
+# Load dataset
 data_url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
 data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
@@ -25,7 +25,6 @@ y = boston_df['MEDV']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-
 # Get hyperparameters from command line
 import argparse
 parser = argparse.ArgumentParser()
@@ -34,10 +33,6 @@ parser.add_argument("--max_depth", type=int, required=True)
 parser.add_argument("--learning_rate", type=float, required=True)
 parser.add_argument("--subsample", type=float, required=True)
 args = parser.parse_args()
-
-# Simulate command-line arguments
-#args = parser.parse_args(['--n_estimators', '100', '--max_depth', '3', '--learning_rate', '0.1', '--subsample', '0.8'])
-# MSE: 5.8546915926708305
 
 # Train model
 model = XGBRegressor(
@@ -51,18 +46,12 @@ model.fit(X_train, y_train)
 # Evaluate
 y_pred = model.predict(X_test)
 mse = mean_squared_error(y_test, y_pred)
-print(f"MSE: {mse}\n")
-
+print(f"MSE: {mse}")
 
 # Save model and metrics
+model_filename = f"/data/model_{mse}.joblib"
+metrics_filename = f"/data/metrics_{mse}.txt"
 
-joblib.dump(model, f"/data/model_{args.n_estimators}_{args.max_depth}.joblib")
-with open(f"/data/metrics_{args.n_estimators}_{args.max_depth}.txt", "w") as f:
-    f.write(str(mse)+ "\n")
-    """
-
-# Save model and metrics in the current directory
-joblib.dump(model, f"model_{args.n_estimators}_{args.max_depth}.joblib")
-with open(f"metrics_{args.n_estimators}_{args.max_depth}.txt", "w") as f:
+joblib.dump(model, model_filename)
+with open(metrics_filename, "w") as f:
     f.write(str(mse))
-    """
